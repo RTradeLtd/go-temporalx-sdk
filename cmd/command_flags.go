69ee49cb7ecdc5a78e3bf00713c3445eeea77fdb
postables <postables@rtradetechnologies.com>
2020-04-01 03:36:41 -0700

    update flags

--- cmd/command_flags.go
+++ cmd/command_flags.go
@@ -9,6 +9,7 @@ func multiAddrFlag(usage string) *cli.StringFlag {
 		Name:    "multi.address",
 		Aliases: []string{"ma"},
 		Usage:   usage,
+		EnvVars: []string{"MULTI_ADDRESS", "MA"},
 	}
 }
 
@@ -17,13 +18,15 @@ func peerIDFlag(usage string) *cli.StringFlag {
 		Name:    "peer.id",
 		Aliases: []string{"pid"},
 		Usage:   usage,
+		EnvVars: []string{"PEER_ID", "PID"},
 	}
 }
 
 func cidFlag(usage string) *cli.StringFlag {
 	return &cli.StringFlag{
-		Name:  "cid",
-		Usage: usage,
+		Name:    "cid",
+		Usage:   usage,
+		EnvVars: []string{"CID"},
 	}
 }
 
@@ -32,14 +35,16 @@ func printProgressFlag() *cli.BoolFlag {
 		Name:    "print.progress",
 		Aliases: []string{"pp"},
 		Usage:   "print progress of uploads/downloads",
+		EnvVars: []string{"PRINT_PROGRESS", "PP"},
 	}
 }
 
 func outputFlag() *cli.StringFlag {
 	return &cli.StringFlag{
-		Name:  "output",
-		Usage: "control output, accepts 'print' or 'monitor'",
-		Value: "print",
+		Name:    "output",
+		Usage:   "control output, accepts 'print' or 'monitor'",
+		Value:   "print",
+		EnvVars: []string{"OUTPUT"},
 	}
 }
 
@@ -48,6 +53,7 @@ func keyName() *cli.StringFlag {
 		Name:    "key.name",
 		Aliases: []string{"kn"},
 		Usage:   "name of the key used in the keystore",
+		EnvVars: []string{"KEY_NAME", "KN"},
 	}
 }
 
@@ -56,6 +62,7 @@ func keyType() *cli.StringFlag {
 		Name:    "key.type",
 		Aliases: []string{"kt"},
 		Usage:   "type of key: ed25519, ecdsa, rsa, secp256k1",
+		EnvVars: []string{"KEY_TYPE", "KT"},
 	}
 }
 
@@ -64,6 +71,7 @@ func keySize() *cli.IntFlag {
 		Name:    "key.size",
 		Aliases: []string{"ks"},
 		Usage:   "size of key in bytes",
+		EnvVars: []string{"KEY_SIZE", "KS"},
 	}
 }
 
@@ -73,6 +81,7 @@ func mnemonicFlag() *cli.StringFlag {
 		Aliases: []string{"sm"},
 		Usage:   "save mnemonic to `PATH` if not empty",
 		Value:   "",
+		EnvVars: []string{"SAVE_MNEMONIC", "SM"},
 	}
 }
 
@@ -81,6 +90,7 @@ func inputFileFlag() *cli.StringFlag {
 		Name:    "input.file",
 		Aliases: []string{"in.fi", "if"},
 		Usage:   "load data contained in file at `PATH`",
+		EnvVars: []string{"INPUT_FILE", "IF"},
 	}
 }
 
@@ -93,36 +103,44 @@ func p2pFlags(cmdFlag *cli.StringFlag) []cli.Flag {
 	}
 	return append([]cli.Flag{
 		&cli.BoolFlag{
-			Name:  "all",
-			Usage: "close all listeners. used by: close",
+			Name:    "all",
+			Usage:   "close all listeners. used by: close",
+			EnvVars: []string{"ALL"},
 		},
 		&cli.BoolFlag{
-			Name:  "verbose",
-			Usage: "print protocol, listen and target information. used by ls",
+			Name:    "verbose",
+			Usage:   "print protocol, listen and target information. used by ls",
+			EnvVars: []string{"VERBOSE"},
 		},
 		&cli.BoolFlag{
-			Name:  "custom.protocols",
-			Usage: "disables requiring /x/ prefix. used by: listen, forward",
+			Name:    "custom.protocols",
+			Usage:   "disables requiring /x/ prefix. used by: listen, forward",
+			EnvVars: []string{"CUSTOM_PROTOCOLS"},
 		},
 		&cli.BoolFlag{
-			Name:  "report.peerid",
-			Usage: "send base58 peerID to target. used by: listen",
+			Name:    "report.peerid",
+			Usage:   "send base58 peerID to target. used by: listen",
+			EnvVars: []string{"REPORT_PEERID"},
 		},
 		&cli.StringFlag{
-			Name:  "protocol.name",
-			Usage: "match/set protocol name. used by: close, forward, listen",
+			Name:    "protocol.name",
+			Usage:   "match/set protocol name. used by: close, forward, listen",
+			EnvVars: []string{"PROTOCOL_NAME"},
 		},
 		&cli.StringFlag{
-			Name:  "listen.address",
-			Usage: "match/set against listen address. used by: close, forward",
+			Name:    "listen.address",
+			Usage:   "match/set against listen address. used by: close, forward",
+			EnvVars: []string{"LISTEN_ADDRESS"},
 		},
 		&cli.StringFlag{
-			Name:  "target.address",
-			Usage: "match/set against target address. used by: close, forward, listen",
+			Name:    "target.address",
+			Usage:   "match/set against target address. used by: close, forward, listen",
+			EnvVars: []string{"TARGET_ADDRESS"},
 		},
 		&cli.StringFlag{
-			Name:  "remote.address",
-			Usage: "note currently used but here for compatability",
+			Name:    "remote.address",
+			Usage:   "note currently used but here for compatability",
+			EnvVars: []string{"REMOTE_ADDRESS"},
 		},
 	}, cmdFlag)
 }
