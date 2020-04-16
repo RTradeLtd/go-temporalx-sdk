04504eb0b69c0b619ccbbcaec5d6323c11be6da4
postables <postables@rtradetechnologies.com>
2020-04-16 00:12:00 -0700

    fix build script

--- .scripts/cli.sh
+++ .scripts/cli.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 TARGETS="linux/amd64 linux/386 linux/arm linux/arm64 darwin/amd64 freebsd/amd64 freebsd/386 freebsd/arm freebsd/arm64 openbsd/amd64 openbsd/386 openbsd/arm openbsd/arm64"
+DATE=`date +%Y.%m.%d.%H%M%S`
+VERSION=`git describe --tags`
 
 # Cross-compile Temporal using gox, injecting appropriate tags.
 go get -u github.com/mitchellh/gox
@@ -9,7 +11,7 @@ rm -rf release
 mkdir -p release
 
 gox -output="release/tex-cli-$(git describe --tags)-{{.OS}}-{{.Arch}}" \
-    -ldflags "-X main.Version=$RELEASE -X main.PublicKey=$PUBLIC_KEY -s -w" \
+    -ldflags "-X main.Version=$VERSION -X main.CompileDate=$DATE -s -w" \
     -osarch="$TARGETS" \
     ./cmd/tex
 
